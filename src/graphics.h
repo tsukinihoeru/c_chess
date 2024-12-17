7ce40e057f2889b059b5c48ffa6995bebfd8ab3a
Harry Chiu <chiuharry0716@gmail.com>
2024-12-17 16:52:35 -0600

    Added ASCII art to draw board

--- src/graphics.h
+++ src/graphics.h
@@ -1,13 +1,64 @@
 #include <ncurses.h>
+#include <wchar.h>
 #ifndef GRAPHICS_H
 #define GRAPHICS_H
+
+static const short COLOR_GREY = 14;
 
 static const short BLACK_PIECE_DARK_SQUARE_C = 2;
 static const short BLACK_PIECE_LIGHT_SQUARE_C = 3;
 static const short WHITE_PIECE_DARK_SQUARE_C = 4;
 static const short WHITE_PIECE_LIGHT_SQUARE_C = 5;
 
+static const int SQUARE_SIZE_VERT = 4;
+
 void init_colors();
-void draw_board(WINDOW * win, int * mailbox);
+void draw_board(WINDOW *win, int *mailbox);
+
+static const char piece_art[6][6][15] = {
+    {
+        "             ",
+        "     _       ",
+        "    (_)      ",
+        "   (___)     ",
+        "   _|_|_     ",
+        "  (_____)    "
+    },{
+        "    |\\_      ",
+        "   /  .\\_    ",
+        "  |   ___)   ",
+        "   \\ = |     ",
+        "   /____\\    ",
+        "  [______]   "
+    },{
+        "     _0      ",
+        "   / //\\     ",
+        "  {     }    ",
+        "   \\   /     ",
+        "   _|_|_     ",
+        "  (_____)    "
+    },{
+        "  __  _  _   ",
+        " |________|  ",
+        " \\__ ____ /  ",
+        "  |____|_|   ",
+        "  |__|___|   ",
+        " [________]  "
+    },{
+        "    _()_     ",
+        "  _/____\\_   ",
+        "  \\______/   ",
+        "   (____)    ",
+        "    |__|     ",
+        "  (______)   "
+    },{
+        "    _++_     ",
+        "  _/____\\_   ",
+        "  \\______/   ",
+        "   (____)    ",
+        "    |__|     ",
+        "  (______)   "
+    }
+};
 
 #endif
--- src/main.c
+++ src/main.c
@@ -1,24 +1,44 @@
 #include "bitboard.h"
 #include "graphics.h"
 
+void draw_b(int *mailbox) {
+    for (int i = 7; i >= 0; i--) {
+        for(int k = 0; k < 6; k++){
+            for (int j = 0; j < 8; j++) {
+                int piece = mailbox[i * 8 + j];
+                if(piece){
+                    for(int ch = 0; ch < 12; ch++){
+                        printf("%c", piece_art[(piece >> 1) - 2][k][ch]);
+                    }
+                }
+                else{
+                    printf("            ");
+                }
+            }printf("\n");
+        } 
+    }
+};
+
 int main(){
     WINDOW * win;
     Board board;
-    char str [80];
+    //char str [80];
     char fen[] = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
     parse_board(&board, fen);
     print_board(&board);
+    
     initscr();
     clear();
     start_color();
     init_colors();
     refresh();
-    win = newwin(8, 28, 2, 2);
+    win = newwin(60, 100, 2, 2);
     mvwprintw(win, 0, 0, "PLEASE");
     wrefresh(win);
     getch();
     draw_board(win, board.mailbox);
     getch();
     endwin();
+    
     return 0;
 }
